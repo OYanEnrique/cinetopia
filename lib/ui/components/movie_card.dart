bde844c19ceef84f9aeb1f8d646009881d95be4e
OYanEnrique <enrique4work@gmail.com>
2025-12-17 16:35:41 -0300

    feat: cria componente MovieCard para exibir informações dos filmes

---
+++ lib/ui/components/movie_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MovieCard extends StatelessWidget {
+  const MovieCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: 
+        <Widget>[
+          Container(
+            width: 90,
+            height: 120,
+            decoration: BoxDecoration(
+              color: Color(0xFF000000),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            margin: const EdgeInsets.only(right: 16),
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 6),
+              child: Text("Título do Filme", 
+                style: TextStyle(
+                  fontSize:18, 
+                  fontWeight: FontWeight.w600,
+                  ),
+                  ),
+            ),
+            Text("Lançamento: 2025-01-01",
+              style: TextStyle(
+                color: Color(0xFFA5A5A5),
+              )),
+          ],
+          ),
+        ],
+        );
+  }
+}
