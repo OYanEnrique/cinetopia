0208d9800c38405d11c6c851d499354077d76c9c
OYanEnrique <enrique4work@gmail.com>
2025-12-18 16:12:40 -0300

    feat: implementa service de busca de filmes populares

---
+++ lib/app/services/search_movies_service.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:cinetopia/app/models/movie.dart';
+import 'package:http/http.dart' as http;
+import '../api_key.dart';
+
+class SearchMoviesService {
+  final String popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+  List<Movie> movies = <Movie>[];
+
+  Future<List<Movie>> searchMovies() async {
+    try{
+      final response = await http.get(Uri.parse(popularMoviesUrl), 
+      headers: {
+        'accept': 'application/json',
+        'Authorization': 'Bearer $apiKey',
+      },
+      );
+      if (response.statusCode == 200) {
+        for (dynamic movie in json.decode(response.body)['results']) {
+          movies.add(Movie.fromMap(movie));
+        }
+      }
+      else {
+        throw Exception(response.body);
+      }
+      return movies;
+    } catch (e) {
+      print(e);
+      return movies;
+      
+    }
+  }
+}
