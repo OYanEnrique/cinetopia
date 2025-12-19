0986728bb5b6fa6fe5cbf81b1fe22f53688bfd16
OYanEnrique <enrique4work@gmail.com>
2025-12-19 15:47:14 -0300

    feat: integra MovieCard com dados reais da API exibindo poster e informações do filme

--- lib/ui/components/movie_card.dart
+++ lib/ui/components/movie_card.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cinetopia/app/models/movie.dart';
+import 'package:flutter/material.dart';
 
 class MovieCard extends StatelessWidget {
-  const MovieCard({super.key});
+  final Movie movie;
 
+  const MovieCard({super.key, required this.movie});
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -14,6 +17,10 @@ class MovieCard extends StatelessWidget {
             decoration: BoxDecoration(
               color: Color(0xFF000000),
               borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(
+                image: NetworkImage(movie.getPosterImage()),
+                fit: BoxFit.cover,
+              ),
             ),
             margin: const EdgeInsets.only(right: 16),
           ),
@@ -23,14 +30,14 @@ class MovieCard extends StatelessWidget {
             children: <Widget>[
             Padding(
               padding: const EdgeInsets.only(bottom: 6),
-              child: Text("Título do Filme", 
+              child: Text(movie.title, 
                 style: TextStyle(
                   fontSize:18, 
                   fontWeight: FontWeight.w600,
                   ),
                   ),
             ),
-            Text("Lançamento: 2025-01-01",
+            Text("Lançamento: ${movie.releaseDate}",
               style: TextStyle(
                 color: Color(0xFFA5A5A5),
               )),
