6f5e73d5e461705c50ccb4fa6418488c57348dc2
OYanEnrique <enrique4work@gmail.com>
2025-12-20 16:29:16 -0300

    feat: adiciona pesquisa dinâmica de filmes

--- lib/ui/screens/search_movies.dart
+++ lib/ui/screens/search_movies.dart
@@ -1,23 +1,35 @@
-import 'package:cinetopia/app/services/search_movies_service.dart';
 import 'package:cinetopia/app/viewmodels/search_movies_viewmodel.dart';
 import 'package:cinetopia/ui/components/movie_card.dart';
 import 'package:flutter/material.dart';
+import 'package:cinetopia/ui/screens/movie_details.dart';
 
-class SearchMovies extends StatelessWidget {
+class SearchMovies extends StatefulWidget {
+  @override
+  State<SearchMovies> createState() => _SearchMoviesState();
+}
+
+class _SearchMoviesState extends State<SearchMovies> {
   final SearchMoviesViewmodel viewmodel = SearchMoviesViewmodel();
+
+  final TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future : viewmodel.getPopularMovies(),
+        future : viewmodel.getMovie(textController.text),
         builder: (context, snapshot){
           if (snapshot.hasData) {
-            return CustomScrollView(
-              slivers: <Widget>[
-                SliverToBoxAdapter(child: Image.asset("assets/movie.png", height: 80, width: 80,),),
-                SliverToBoxAdapter(child: Padding(
-                  padding: const EdgeInsets.only(bottom: 40),
-                  child: Text("Filmes populares",
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: CustomScrollView(
+                slivers: <Widget>[
+                  SliverToBoxAdapter(child: Padding(
+                    padding: const EdgeInsets.only(top: 16.0),
+                    child: Image.asset("assets/movie.png", height: 80, width: 80,),
+                  ),),
+                  SliverToBoxAdapter(child: Padding(
+                    padding: const EdgeInsets.only(bottom: 40),
+                    child: Text("Filmes populares",
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 24,
@@ -29,6 +41,11 @@ class SearchMovies extends StatelessWidget {
                 SliverToBoxAdapter(child: Padding(
                   padding: const EdgeInsets.only(bottom: 32),
                   child: TextField(
+                    controller:textController,
+                    onEditingComplete: () {
+                      FocusScope.of(context).unfocus();
+                      setState(() {});
+                    },
                     decoration: InputDecoration(
                       hintText: 'Pesquisar',
                       border: OutlineInputBorder(
@@ -38,15 +55,27 @@ class SearchMovies extends StatelessWidget {
                   ),
                 ),
                 ),
-                SliverList.builder(itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.only(bottom: 32),
-                  child: MovieCard(
-                    movie: viewmodel.moviesList[index],
-                  ),
-                ), 
+                SliverList.builder(
+                  itemBuilder: (context, index) => InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MovieDetails(movie: viewmodel.moviesList[index]),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                    padding: const EdgeInsets.only(bottom: 32),
+                    child: MovieCard(
+                      movie: viewmodel.moviesList[index],
+                    ),
+                                    ),
+                  ), 
                 itemCount: viewmodel.moviesList.length,
                 ),
               ]
+              ),
             );
           } else {
           return Center(child: CircularProgressIndicator());
